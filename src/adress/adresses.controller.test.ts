0f6eff35b1772fb193b704dbddbe2f8427456442
Olivier R.Bernier <olivier.rbernier.1@ens.etsmtl.ca>
2018-07-30 21:04:33 -0400

    Tests 2.0 for adresses

--- src/adress/adresses.controller.test.ts
+++ src/adress/adresses.controller.test.ts
@@ -1,47 +1,111 @@
+import { Test, TestingModule } from '@nestjs/testing';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { Repository } from '../../node_modules/typeorm';
 import { AdressesController } from './adresses.controller';
 import { AdressesService } from './adresses.service';
 import { Adress } from './adress.entity';
 
+
 describe('AdressesController', () => {
     let controller: AdressesController;
     let service: AdressesService;
-    let repo: Repository<Adress>;
-    
+    let id: number;
 
-    beforeEach(() => {
-        repo = new Repository<Adress>();
-        service = new AdressesService(repo);
-        controller = new AdressesController(service);
+    beforeAll(async () => {
+        const mod: TestingModule = await Test.createTestingModule({
+            imports: [
+                TypeOrmModule.forRoot({
+                    "type": "mysql",
+                    "host": "127.0.0.1",
+                    "port": 3306,
+                    "username": "root",
+                    "password": "root",
+                    "database": "back-end",
+                    "entities": [Adress],
+                    "synchronize": false
+                  }),
+                TypeOrmModule.forFeature([Adress])
+            ],
+            controllers:[
+                AdressesController
+            ],
+            providers:[
+                AdressesService
+            ],
+            components: [
+                {
+                    provide: 'UserRepository',
+                    useClass: Repository
+                }
+            ]
+        }).compile();
+        service = mod.get<AdressesService>(AdressesService);
+        controller = mod.get<AdressesController>(AdressesController);
     });
 
-    describe('Test adresses', () => {
-        it('Should test the adresses controller, service and entity.', async () => {
+    describe('findAll', () => {
+        it('Should return all adresses.', async ()=> {
+            const result = [];
+            jest.spyOn(service, 'findAll').mockImplementation(()=>result);
+            expect(await controller.findAll()).toBe(result);
+        });
+    });
 
-            let text = await '{"DoorNumber": 200, "Street": "Georges VI", "City": "Terrebonne", "Province": "Qc", "PostalCode": "J6Y1P1"}';
-            let dto = await JSON.parse(text);
-            let id = await controller.create(dto);
+    describe('create', () => {
+        it('Should return the id of the created adress', async () => {
+            let text = '{"DoorNumber": 200, "Street": "Georges VI", "City": "Terrebonne", "Province": "Qc", "PostalCode": "J6Y1P1"}';
+            let dto = JSON.parse(text);
+            id = await controller.create(dto);
+
             let result = await controller.findOneById(id);
+            expect(result.DoorNumber).toBe(200);
+            expect(result.Street).toBe("Georges VI");
+            expect(result.City).toBe("Terrebonne");
+            expect(result.Province).toBe("Qc");
+            expect(result.PostalCode).toBe("J6Y1P1");
 
-            await expect(result.DoorNumber).toBe(200);
-            await expect(result.Street).toBe("Georges VI");
-            await expect(result.City).toBe("Terrebonne");
-            await expect(result.Province).toBe("Qc");
-            await expect(result.PostalCode).toBe("J6Y1P1");
+            text = '{"Id": '+ id +'}';
+            dto = JSON.parse(text);
+            await controller.delete(dto)
+        })
+    })
 
-            text = await '{"DoorNumber": 250, "Street": "Georges VI", "City": "Terrebonne", "Province": "Qc", "PostalCode": "J6Y1P1"}';
-            dto = await JSON.parse(text);
-            await controller.update(dto);
-            result = await controller.findOneById(id);
-
-            await expect(result.DoorNumber).toBe(250);
-
-            text = await '{"Id":'+ id +'}';
-            dto = await JSON.parse(text);
-            await controller.delete(dto);
-            let Allresult = await controller.findAll();
+    describe('findOneById', () => {
+        it('Should return adress with Id = id', async ()=>{
             
-            await expect(Allresult).toBe([]);
+            
+            const text = '{"DoorNumber": 200, "Street": "Georges VI", "City": "Terrebonne", "Province": "Qc", "PostalCode": "J6Y1P1"}';
+            const dto = JSON.parse(text);
+            id = await controller.create(dto);
+
+            let result = await controller.findOneById(id);
+            expect(result.Id).toBe(id);
+        });
+    });
+
+    describe('update', () => {
+        it('Should update the adress with the id', async ()=>{
+            
+            
+            const text = '{"Id": '+ id +', "DoorNumber": 250, "Street": "Georges VI", "City": "Terrebonne", "Province": "Qc", "PostalCode": "J6Y1P1"}';
+            const dto = JSON.parse(text);
+            await controller.update(dto);
+
+            let result = await controller.findOneById(id);
+            expect(result.DoorNumber).toBe(250);
+        });
+    });
+
+    describe('delete', () => {
+        it('Should delete adress with the id', async ()=>{
+            
+            
+            const text = '{"Id": '+ id +'}';
+            const dto = JSON.parse(text);
+            controller.delete(dto);
+
+            let result = await controller.findOneById(id);
+            expect(result);
         });
     });
 });
